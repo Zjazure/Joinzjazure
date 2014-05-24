513c249aacd4619fd4e0889282c2ac2840d1926c
Henry Zhu <Henry.Zhu.1997@outlook.com>
2014-05-24 12:57:14 +0800

    Fixed group selection bug, added group info for program group and resource group

--- Joinzjazure/Scripts/Home.js
+++ Joinzjazure/Scripts/Home.js
@@ -1,13 +1,13 @@
 ﻿$('#Groups').val(0)
 
 function handleCheck(id, value) {
-    var g = '#Groups';
+    var g = $('#Groups');
     if ($(id).is(':checked') == true) {
-        var val = Number($(g).val()) | Number(value);
+        var val = Number(g.val()) | Number(value);
         $(g).val(val);
     }
     else {
-        var val = Number($(g).val()) & (!Number(value));
+        var val = Number(g.val()) & (~Number(value));
         $(g).val(val);
     }
 }
