e2080f52c0315e3b1e8f3c03a078824aaff22ece
Henry Charles <HenryTech@outlook.com>
2014-07-14 11:37:55 +0800

    moved scripts to buttom of the page

--- Joinzjazure/App_Start/BundleConfig.cs
+++ Joinzjazure/App_Start/BundleConfig.cs
@@ -7,22 +7,14 @@ namespace Joinzjazure
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/val").Include(
-                    "~/Scripts/jquery.unobtrusive.min.js",
-                    "~/Scripts/jquery.validate.min.js",
-                    "~/Scripts/jquery.validate.unobtrusive.min.js",
-                    "~/Scripts/Home.js"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Scripts/jquery-{version}.min.js",
+                        "~/Scripts/jquery.unobtrusive.min.js",
+                        "~/Scripts/jquery.validate.min.js",
+                        "~/Scripts/jquery.validate.unobtrusive.min.js",
+                        "~/Scripts/modernizr-*",
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Scripts/Home.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.cosmo.min.css",
