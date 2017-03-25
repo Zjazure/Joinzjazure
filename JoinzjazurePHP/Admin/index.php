2ffa0f10d10182e64195583d0d414669a9046b89
SamLangTen <samlangten@outlook.com>
2017-03-25 18:59:22 +0800

    add disable website function

--- JoinzjazurePHP/Admin/index.php
+++ JoinzjazurePHP/Admin/index.php
@@ -11,39 +11,48 @@ require(dirname(__DIR__)."/class/member.class.php");
 <body>
     <div class="container" style="background-color:rgba(255,255,255,0.83)">
         <div class="page-header">
-            <h1>湛江一中IT社网络报名系统 管理后台<a href="logout.php" class="btn btn-info" style="float:right;margin-left:5px;">注销</a><a class="btn btn-info" style="float:right;" data-toggle="modal" data-target="#editDBModel">配置数据库</a></h1>
+            <h1>湛江一中IT社网络报名系统 管理后台<a href="logout.php" class="btn btn-info" style="float:right;margin-left:5px;">注销</a><a class="btn btn-info" style="float:right;" data-toggle="modal" data-target="#editDBModel">配置</a></h1>
         </div>
         <!--Dialog-->
         <div class="modal fade" id="editDBModel" tabindex="-1" role="dialog" aria-labelledby="editDBModel">
             <?php $conf = config::get_configs();?>
             <div class="modal-dialog" role="document">
-                <form method="POST" action="editdb.php">
+                <form method="POST" action="editconfig.php">
                     <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title" id="EditDatabaseTitle">编辑数据库连接</h4>
+                        <h4 class="modal-title" id="EditDatabaseTitle">配置</h4>
                     </div>
-                    <div class="modal-body">         
-                            <div class="form-group">
-                                <label for="database-host" class="control-label">数据库主机</label>
-                                <input type="text" class="form-control" id="database-host" name="database-host" value="<?php if(isset($conf["database_host"]))echo($conf["database_host"]);?>">
-                            </div>
-                            <div class="form-group">
-                                <label for="database-name" class="control-label">数据库名称</label>
-                                <input type="text" class="form-control" id="database-name" name="database-name" value="<?php if(isset($conf["database_name"]))echo($conf["database_name"]);?>">
-                            </div>
-                            <div class="form-group">
-                                <label for="database-table" class="control-label">数据表名</label>
-                                <input type="text" class="form-control" id="database-table" name="database-table" value="<?php if(isset($conf["database_table"]))echo($conf["database_table"]);?>">
-                            </div>
-                            <div class="form-group">
-                                <label for="database-user" class="control-label">用户名</label>
-                                <input type="text" class="form-control" id="database-user" name="database-user" value="<?php if(isset($conf["database_user"]))echo($conf["database_user"]);?>">
-                            </div>
-                            <div class="form-group">
-                                <label for="database-pass" class="control-label">密码</label>
-                                <input type="text" class="form-control" id="database-pass" name="database-pass" value="<?php if(isset($conf["database_pass"]))echo($conf["database_pass"]);?>">
-                            </div>
+                    <div class="modal-body">
+                        <h3>网站配置</h3>
+                        <div class="form-group">
+                            <label for="website-enable" class="control-label">是否启动网站</label>
+                            <select class="form-control" id="website-enable" name="website-enable">
+                            <option value ="Enabled" <?php if(isset($conf["website_enable"])&&($conf["website_enable"]=="Enabled"))echo("selected=\"selected\"");?>>启动</option>
+                            <option value ="Disabled" <?php if(isset($conf["website_enable"])&&($conf["website_enable"]=="Disabled"))echo("selected=\"selected\"");?>>关闭</option>
+                            </select>
+                        </div>
+                        <h3>数据库配置</h3>
+                        <div class="form-group">
+                            <label for="database-host" class="control-label">数据库主机</label>
+                            <input type="text" class="form-control" id="database-host" name="database-host" value="<?php if(isset($conf["database_host"]))echo($conf["database_host"]);?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="database-name" class="control-label">数据库名称</label>
+                            <input type="text" class="form-control" id="database-name" name="database-name" value="<?php if(isset($conf["database_name"]))echo($conf["database_name"]);?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="database-table" class="control-label">数据表名</label>
+                            <input type="text" class="form-control" id="database-table" name="database-table" value="<?php if(isset($conf["database_table"]))echo($conf["database_table"]);?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="database-user" class="control-label">用户名</label>
+                            <input type="text" class="form-control" id="database-user" name="database-user" value="<?php if(isset($conf["database_user"]))echo($conf["database_user"]);?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="database-pass" class="control-label">密码</label>
+                            <input type="text" class="form-control" id="database-pass" name="database-pass" value="<?php if(isset($conf["database_pass"]))echo($conf["database_pass"]);?>">
+                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
--- JoinzjazurePHP/submit.php
+++ JoinzjazurePHP/submit.php
@@ -10,25 +10,33 @@ require("header.php");
     <h1>湛江一中IT社 网络报名系统</h1>
 </div>
 <?php 
+$conf = config::get_configs();
 $anouncements = json_decode(file_get_contents(__DIR__."/JsonData/Anouncement.json"),true);
-if(isset($anouncements["Submit"]))echo($anouncements["Submit"]) 
+if(isset($conf["website_enable"])&&$conf["website_enable"]=="Enabled")
+    {if(isset($anouncements["Submit"])) echo($anouncements["Submit"]);}
+else
+    {if(isset($anouncements["DisabledSubmit"])) echo($anouncements["DisabledSubmit"]);}
 ?>
 
 <?php
-    $member = new member();
-    $member->name = $_POST["Name"];
-    $member->gender = $_POST["Gender"]=="True"?true:false;
-    $member->grade = $_POST["Grade"];
-    $member->class = $_POST["Class"];
-    $member->groups = isset($_POST["counter"])?$_POST["counter"]:array("None");
-    $member->email = $_POST["Email"];
-    $member->phone = $_POST["Phone"];
-    $member->QQ = $_POST["QQ"];
-    $member->weibo = $_POST["Weibo"];
-    $member->description = $_POST["Description"];
-    $result = member::add_member($member);
-    if(!$result) die("Failed to add data");
-    unset($_SESSION["VerificationCodeId"]);
+    
+    if(isset($conf["website_enable"])&&$conf["website_enable"]=="Enabled")
+    {
+        $member = new member();
+        $member->name = $_POST["Name"];
+        $member->gender = $_POST["Gender"]=="True"?true:false;
+        $member->grade = $_POST["Grade"];
+        $member->class = $_POST["Class"];
+        $member->groups = isset($_POST["counter"])?$_POST["counter"]:array("None");
+        $member->email = $_POST["Email"];
+        $member->phone = $_POST["Phone"];
+        $member->QQ = $_POST["QQ"];
+        $member->weibo = $_POST["Weibo"];
+        $member->description = $_POST["Description"];
+        $result = member::add_member($member);
+        if(!$result) die("Failed to add data");
+    }
+    unset($_SESSION["VerificationCodeId"]);  
 ?>
 <?php require("footer.php"); ?>
 <?php } ?>
